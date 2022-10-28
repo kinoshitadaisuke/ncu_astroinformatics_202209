0ccb548a8b89f8a80d7c3850dde3bc46e0c40620
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2022-10-28 17:48:08 +0800

    Add files via upload

--- s07/ai202209_s07_21.py
+++ s07/ai202209_s07_21.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.9
 
 #
-# Time-stamp: <2022/10/28 17:24:09 (CST) daisuke>
+# Time-stamp: <2022/10/28 17:47:13 (CST) daisuke>
 #
 
 # importing astropy module
@@ -9,8 +9,9 @@ import astropy.constants
 import astropy.units
 
 # units
-u_ABmag       = astropy.units.ABmag
-u_Jy          = astropy.units.Jy
+u_ABmag = astropy.units.ABmag
+u_Jy    = astropy.units.Jy
+u_uJy   = astropy.units.uJy
 
 # V-band magnitude of 0 in AB magnitude system
 m_V = 0.0 * u_ABmag
@@ -20,7 +21,7 @@ m_K = 20.0 * u_ABmag
 
 # conversion of m_K = 20.0 into Jy
 flux_density_V = m_V.to (u_Jy)
-flux_density_K = m_K.to (u_Jy)
+flux_density_K = m_K.to (u_uJy)
 
 # printing result
 print (f'm_V = {m_V:4.1f}  ==>  flux density = {flux_density_V:g}')
