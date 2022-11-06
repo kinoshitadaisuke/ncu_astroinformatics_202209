368dcc51e52fdf5ab4beea47fbf37e7c06dce031
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2022-11-06 20:18:27 +0800

    Add files via upload

---
+++ s08/ai202209_s08_01_19.py
@@ -0,0 +1,46 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2022/11/06 16:28:40 (CST) daisuke>
+#
+
+# importing astropy module
+import astropy.coordinates
+import astropy.time
+import astropy.units
+
+# importing astroplan module
+import astroplan
+
+# units
+u_m = astropy.units.m
+
+# using "DE440" for solar system ephemeris
+astropy.coordinates.solar_system_ephemeris.set ('de440')
+
+# location of observer: NCU main campus
+longitude = '121d11m12s'
+latitude  = '+24d58m12s'
+height    = 151.6 * u_m
+
+# making a location object using Astropy
+location = astropy.coordinates.EarthLocation.from_geodetic (lon=longitude, \
+                                                            lat=latitude, \
+                                                            height=height)
+
+# making an observer object
+observer = astroplan.Observer (location=location, name='NCU', \
+                               timezone='Asia/Taipei')
+
+# printing created observer object
+print (observer)
+
+# start of morning twilight
+twilight_morning_guess = astropy.time.Time ('2022-11-07 22:00:00', \
+                                            format='iso', scale='utc')
+twilight_morning = observer.twilight_morning_astronomical \
+    (twilight_morning_guess, which='nearest', n_grid_points=500)
+
+# printing start of morning twilight
+print (f'start of morning twilight = JD {twilight_morning.jd}', \
+       f'= {twilight_morning.iso}')
