e7848a181a9eb02b6313b82f94a2ab53bae7da50
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2022-11-10 09:52:21 +0800

    Add files via upload

---
+++ s09/ai202209_s09_00_00.py
@@ -0,0 +1,26 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2022/11/10 09:51:23 (CST) daisuke>
+#
+
+# importing scipy module
+import scipy.constants
+
+#
+# constants
+#
+
+# speed of light
+c = scipy.constants.c
+
+# Planck constant
+h = scipy.constants.h
+
+# Boltzmann constant
+k = scipy.constants.k
+
+# printing values of constants
+print (f'c = {c:g}')
+print (f'h = {h:g}')
+print (f'k = {k:g}')
