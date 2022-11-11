953d3aa203d211b2e0d4d3808175f95e052e1188
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2022-11-11 13:52:52 +0800

    Add files via upload

---
+++ s09/ai202209_s09_00_07.py
@@ -0,0 +1,47 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2022/11/11 13:50:46 (CST) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing scipy module
+import scipy.constants
+import scipy.optimize
+
+# importing matplotlib module
+import matplotlib.figure
+import matplotlib.backends.backend_agg
+
+#
+# function to calculate blackbody curve
+#
+def bb_lambda (wavelength, T):
+    # speed of light
+    c = scipy.constants.physical_constants['speed of light in vacuum']
+
+    # Planck constant
+    h = scipy.constants.physical_constants['Planck constant']
+
+    # Boltzmann constant
+    k = scipy.constants.physical_constants['Boltzmann constant']
+
+    # calculation of Planck function
+    blackbody = 2.0 * h[0] * c[0]**2 / wavelength**5 \
+        / (numpy.exp (h[0] * c[0] / (wavelength * k[0] * T) ) - 1.0 )
+
+    # returning blackbody curve
+    return (blackbody * -1.0)
+
+# temperature of blackbody
+T = 2500.0
+
+# finding a peak of T=5800 K blackbody radiation spectrum
+wavelength_peak = scipy.optimize.minimize_scalar (bb_lambda, \
+                                                  bracket=(10**-8, 10**-3), \
+                                                  args=(T), method='Brent')
+
+# printing peak wavelength of black body radiation
+print (f'peak wavelength of T={T} K blackbody = {wavelength_peak.x} m')
