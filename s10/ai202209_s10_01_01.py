8d61422c25991c5a549f2b5938160499829b9338
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2022-11-20 21:22:50 +0800

    Add files via upload

---
+++ s10/ai202209_s10_01_01.py
@@ -0,0 +1,45 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2022/11/20 19:32:21 (CST) daisuke>
+#
+
+# importing urllib module
+import urllib.request
+
+# importing ssl module
+import ssl
+
+# allow insecure downloading
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URL of data file
+url_data = 'http://cdsarc.u-strasbg.fr/ftp/V/50/ReadMe'
+
+# output file name
+file_output = 'bsc5.readme'
+
+# printing status
+print (f'Now, fetching {url_data}...')
+
+# opening URL
+with urllib.request.urlopen (url_data) as fh_read:
+    # reading data
+    data_byte = fh_read.read ()
+
+# printing status
+print (f'Finished fetching {url_data}!')
+
+# converting raw byte data into string
+data_str = data_byte.decode ('utf-8')
+
+# printing status
+print (f'Now, writing data into file "{file_output}"...')
+
+# opening file for writing
+with open (file_output, 'w') as fh_write:
+    # writing data
+    fh_write.write (data_str)
+
+# printing status
+print (f'Finished writing data into file "{file_output}"!')
