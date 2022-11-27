950ca5b826c0da40981a832630a1ed34de302799
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2022-11-27 23:04:08 +0800

    Add files via upload

---
+++ s11/ai202209_s11_00_07.py
@@ -0,0 +1,127 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2022/11/27 19:02:43 (CST) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing scipy module
+import scipy.optimize
+
+# importing matplotlib module
+import matplotlib.figure
+import matplotlib.backends.backend_agg
+
+# input data file
+file_input = 'ai202209_s11_00_04.data'
+
+# output figure file
+file_output = 'ai202209_s11_00_07.png'
+
+# range of data for fitting
+x_min_fit = 1.9
+x_max_fit = 2.1
+
+# range of data for plotting
+x_min_plot = 1.7
+x_max_plot = 2.3
+
+# empty numpy array for storing data
+data_all_per = numpy.array ([])
+data_all_var = numpy.array ([])
+data_fit_per = numpy.array ([])
+data_fit_var = numpy.array ([])
+
+# opening file for reading
+with open (file_input, 'r') as fh:
+    # reading file line-by-line
+    for line in fh:
+        # skipping line if the line starts with '#'
+        if (line[0] == '#'):
+            continue
+        # removing line feed at the end of line
+        line = line.strip ()
+        # splitting data
+        (per_day_str, per_hr_str, per_min_str, var_str) = line.split ()
+        # conversion from string into float
+        per_hr = float (per_hr_str)
+        var = float (var_str)
+        # appending the data at the end of numpy arrays
+        data_all_per = numpy.append (data_all_per, per_hr)
+        data_all_var = numpy.append (data_all_var, var)
+        if ( (per_hr >= x_min_fit) and (per_hr <= x_max_fit) ):
+            data_fit_per = numpy.append (data_fit_per, per_hr)
+            data_fit_var = numpy.append (data_fit_var, var)
+
+# initial values of coefficients of fitted function
+a = 1.0
+b = 1.0
+c = 1.0
+
+# function to be used for least-squares fitting
+def func (x, a, b, c):
+    y = a * (x - b)**2 + c
+    return y
+
+# least-squares fitting using scipy.optimize.curve_fit
+popt, pcov = scipy.optimize.curve_fit (func, data_fit_per, data_fit_var)
+
+# fitted coefficients
+print ("popt:")
+print (popt)
+
+# covariance matrix
+print ("pcov:")
+print (pcov)
+
+# fitted a and b
+a_fitted = popt[0]
+b_fitted = popt[1]
+c_fitted = popt[2]
+
+# degree of freedom
+dof = len (data_fit_per) - 3
+print (f"dof = {dof}")
+
+# residual
+residual     = data_fit_var - func (data_fit_per, a_fitted, b_fitted, c_fitted)
+reduced_chi2 = (residual**2).sum () / dof
+print (f"reduced chi^2 = {reduced_chi2}")
+
+# errors of a, b, and c
+a_err = numpy.sqrt (pcov[0][0])
+b_err = numpy.sqrt (pcov[1][1])
+c_err = numpy.sqrt (pcov[2][2])
+print (f"a = {a_fitted:8.5f} +/- {a_err:8.5f} ({a_err/a_fitted*100:6.2f} %)")
+print (f"b = {b_fitted:8.5f} +/- {b_err:8.5f} ({b_err/b_fitted*100:6.2f} %)")
+print (f"c = {c_fitted:8.5f} +/- {c_err:8.5f} ({c_err/c_fitted*100:6.2f} %)")
+
+# fitted line
+fitted_x = numpy.linspace (x_min_plot, x_max_plot, 10**3)
+fitted_y = func (fitted_x, a_fitted, b_fitted, c_fitted)
+
+# making fig and ax
+fig    = matplotlib.figure.Figure ()
+canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
+ax     = fig.add_subplot (111)
+
+# labels
+ax.set_xlabel ('Period [hr]')
+ax.set_ylabel ('Variance')
+
+# range of plot
+ax.set_xlim (x_min_plot, x_max_plot)
+
+# plotting a figure
+ax.plot (data_all_per, data_all_var, \
+         linestyle='-', color='blue', linewidth=5, \
+         label='result of PDM analysis')
+ax.plot (fitted_x, fitted_y, \
+         linestyle=':', color='red', linewidth=3, \
+         label='fitted curve by curve_fit')
+ax.legend ()
+
+# saving the figure to a file
+fig.savefig (file_output, dpi=225)
