70df0ebf08a262dd331dab00a0b248893f9e0f17
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2022-12-08 15:09:55 +0800

    Add files via upload

---
+++ s13/ai202209_s13_00_01.py
@@ -0,0 +1,50 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2022/12/08 15:09:27 (CST) daisuke>
+#
+
+# importing argparse module
+import argparse
+
+#
+# command-line argument analysis
+#
+
+# constructing parser object
+desc = f"availability check of Python modules"
+parser = argparse.ArgumentParser (description=desc)
+
+# adding options
+parser.add_argument ('module', type=str, nargs='+', help=f"module name")
+
+# analysis of command-line arguments
+args = parser.parse_args ()
+
+#
+# input parameters
+#
+
+# list of module names for availability check
+list_modules = args.module
+
+#
+# availability check of modules
+#
+
+for module in list_modules:
+    # check of availability of rebound module
+    try:
+        # importing module
+        imported = __import__ (module)
+    except:
+        # if rebound module is not installed, print an error message
+        print (f"The module '{module}' is NOT installed on your computer.")
+    else:
+        # if rebound module is found, print following message
+        print (f"The module '{module}' is found on your computer.")
+        print (f"{imported}")
+    finally:
+        # print that the check of availability of rebound module is finished
+        print (f"A test of availability check of '{module}' module is now finished.")
+        print (f"")
