9991f75c963f3daeb5c4503878fa51fd4acf3cb9
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2022-12-10 01:57:53 +0800

    Add files via upload

---
+++ s13/ai202209_s13_01_01.py
@@ -0,0 +1,36 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2022/12/09 12:43:34 (CST) daisuke>
+#
+
+# importing astropy module
+import astropy.time
+
+# importing astroquery module
+import astroquery.jplhorizons
+
+# target name
+target = 'Sun'
+
+# observatory (D35 ==> Lulin Observatory)
+obs_site = 'D35'
+
+# date/time
+t  = astropy.time.Time ('2023-03-21T04:00:00', scale='utc', format='isot')
+jd = t.jd
+
+# sending a query to NASA/JPL Horizons system
+obj = astroquery.jplhorizons.Horizons (id_type=None, id=target, \
+                                       location=obs_site, epochs=jd)
+
+# printing a query object
+print (obj)
+
+# getting ephemerides
+ephemerides = obj.ephemerides ()
+
+# printing column information of ephemerides
+for col in ephemerides.columns:
+    print (f"{col}")
+    
