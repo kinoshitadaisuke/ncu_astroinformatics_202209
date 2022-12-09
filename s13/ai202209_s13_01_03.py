058d226a7998a82357f7d517237194534962cb27
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2022-12-10 01:58:29 +0800

    Add files via upload

---
+++ s13/ai202209_s13_01_03.py
@@ -0,0 +1,49 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2022/12/09 15:24:20 (CST) daisuke>
+#
+
+# importing astropy module
+import astropy.time
+import astropy.units
+
+# importing astroquery module
+import astroquery.jplhorizons
+
+# units
+u_hr  = astropy.units.hour
+
+# target name
+target = 'Jupiter'
+
+# observatory (D35 ==> Lulin Observatory)
+obs_site = 'D35'
+
+# date/time
+t_start = astropy.time.Time ('2023-01-21T12:00:00', scale='utc', format='isot')
+t_end   = t_start + 6.0 * u_hr
+t_step  = '1h'
+
+# sending a query to NASA/JPL Horizons system
+obj = astroquery.jplhorizons.Horizons (id_type=None, id=target, \
+                                       location=obs_site, \
+                                       epochs={'start': t_start.isot, \
+                                               'stop': t_end.isot, \
+                                               'step': t_step})
+
+# printing a query object
+print (obj)
+
+# getting ephemerides
+ephemerides = obj.ephemerides ()
+
+# printing 'targetname', 'datetime_str', 'RA', 'DEC', 'AZ', 'EL',
+# 'ObsEclLon', 'ObsEclLat', 'GlxLon', 'GlxLat', 'V', 'r', 'delta'
+columns = ['RA', 'DEC', 'AZ', 'EL', 'ObsEclLon', 'ObsEclLat', \
+           'GlxLon', 'GlxLat', 'V', 'r', 'delta']
+print (f"{ephemerides[0]['targetname']}")
+for eph in ephemerides:
+    print (f"  {eph['datetime_str']}")
+    for col in columns:
+        print (f"    {col:12s} = {eph[col]}")
