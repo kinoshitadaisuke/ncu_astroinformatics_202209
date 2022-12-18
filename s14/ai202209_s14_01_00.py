a2fcf68b60a83ac4bd1b5fd59a68e776afc3b223
だいすけ <77160981+kinoshitadaisuke@users.noreply.github.com>
2022-12-18 13:18:47 +0800

    Add files via upload

---
+++ s14/ai202209_s14_01_00.py
@@ -0,0 +1,56 @@
+#!/usr/pkg/bin/python3.9
+
+#
+# Time-stamp: <2022/12/18 10:59:23 (CST) daisuke>
+#
+
+# importing astropy module
+import astropy.units
+import astropy.coordinates
+
+# importing astroquery module
+import astroquery.simbad
+
+# units
+u_ha  = astropy.units.hourangle
+u_deg = astropy.units.deg
+
+# target object name
+target = "Sirius"
+
+# name resolver
+query_result = astroquery.simbad.Simbad.query_object (target)
+
+# coordinate from Simbad
+ra_str  = query_result['RA'][0]
+dec_str = query_result['DEC'][0]
+
+# using skycoord of astropy
+coord = astropy.coordinates.SkyCoord (ra_str, dec_str, frame='icrs', \
+                                      unit=(u_ha, u_deg) )
+
+# coordinate in decimal degree
+ra_deg  = coord.ra.deg
+dec_deg = coord.dec.deg
+
+# coordinate in radian
+ra_rad  = coord.ra.rad
+dec_rad = coord.dec.rad
+
+# coordinate in hexagesimal format
+ra_str  = f"{int (coord.ra.hms.h):02d}h" \
+    + f"{int (coord.ra.hms.m):02d}m" \
+    + f"{coord.ra.hms.s:06.3f}s"
+if (dec_deg < 0.0):
+    dec_str = f"-{int (coord.dec.dms.d * (-1)):02d}d" \
+        + f"{int (coord.dec.dms.m * (-1)):02d}m" \
+        + f"{coord.dec.dms.s * (-1):05.2f}s"
+else:
+    dec_str = f"+{int (coord.dec.dms.d):02d}d" \
+        + f"{int (coord.dec.dms.m):02d}m" \
+        + f"{coord.dec.dms.s:05.2f}s"
+    
+# printing result
+print (f"target: {target}")
+print (f" RA  = {ra_str:13s} = {ra_deg:10.6f} deg = {ra_rad:11.8f} rad")
+print (f" Dec = {dec_str:13s} = {dec_deg:+10.6f} deg = {dec_rad:+10.8f} rad")
